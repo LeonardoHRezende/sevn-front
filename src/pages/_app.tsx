4d0681d422f35cde39aa450a4ef10cda3f8f288f
leonardo.rezende <leonardo.rezende@maisretorno.com>
2024-05-27 22:12:29 -0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import "@sevn/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
